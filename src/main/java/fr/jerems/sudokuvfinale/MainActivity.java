672aedcc89d9c6ae4bf38cfffd3e379b88f1c855
jerems34 <45573207+jerems34@users.noreply.github.com>
2019-04-05 17:28:52 +0200

    Add files via upload
    Version finale marche avec des photos bien centrées et sans flash !

---
+++ src/main/java/fr/jerems/sudokuvfinale/MainActivity.java
@@ -0,0 +1,127 @@
+package fr.jerems.sudokuvfinale;
+
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Size;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static fr.jerems.sudokuvfinale.SudokuSolver.solveImage;
+
+public class MainActivity extends AppCompatActivity {
+    private static Context context;
+    private ImageView imgPhoto;
+    private Button btnphoto;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        context=getApplicationContext();
+        System.loadLibrary("opencv_java3");
+        setContentView(R.layout.activity_main);
+        //System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+
+        initActivity();
+        Log.d("Creation","test132155");
+    }
+
+        private void initActivity(){
+           imgPhoto= (ImageView)findViewById(R.id.imgPhoto);
+           btnphoto= (Button)findViewById(R.id.btnPhoto);
+           createOnClickPhotoButton();
+        }
+
+        private void createOnClickPhotoButton(){
+
+            btnphoto.setOnClickListener(new Button.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //acces a la gallerie
+                    Intent galleryIntent=new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(galleryIntent,1);
+                }
+            });
+        }
+        String currentPhotoPath;
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    public void onActivityResult(int requestCode,int resultCode,Intent data){
+
+       super.onActivityResult(requestCode,resultCode,data);
+        //on verifie si l'image a été recuperee
+        if(requestCode==1 && resultCode==RESULT_OK){
+             //acces a l'image a partir de data
+            Uri selectedImage= data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+            Cursor cursor = this.getContentResolver().query(selectedImage, filePathColumn,null,null,null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String imgPath = cursor.getString(columnIndex);
+        cursor.close();
+
+            Bitmap image = BitmapFactory.decodeFile(imgPath);
+            Log.d("imgPath", imgPath);
+            imgPhoto.setImageBitmap(image);
+            Size size= new Size(344,726);//on redimensionne l'image afin qu'elle pese moins lourd
+            Mat src= Imgcodecs.imread(imgPath);
+            try {
+                File imageFile = createImageFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if(src.height() > 727 && src.width() >345) {
+                Mat dest= new Mat();//dst image
+                String resizePath=imgPath;
+                Imgproc.resize(src,dest,size,344,726,Imgproc.INTER_LINEAR);//resize image
+               Imgcodecs.imwrite(currentPhotoPath, dest);
+                System.out.println("laaaaaaaaaaaaaaaaaaaaa" + currentPhotoPath);
+                SudokuSolver.solveImage(currentPhotoPath);
+            }else {
+
+                SudokuSolver.solveImage(imgPath);
+            }
+            Log.d("finResolution", imgPath);
+        }
+        else{
+            Toast.makeText(this,"aucune image selectione", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+}
+
